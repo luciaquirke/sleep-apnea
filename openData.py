9aa63948148dd9eb56f51f17d173e437aa3a342e
luciaquirke <lqui376@aucklanduni.ac.nz>
2019-07-15 17:16:54 +1200

    added opening and saving data features

--- openData.py
+++ openData.py
@@ -1,32 +1,31 @@
+#NOTES
+#print(annsamp.__dict__)        #can be used to list attributes and values of object
+#fs = sig.fs                    #can be used to find sampling rate of 250 Hz
+
 from IPython.display import display
 import numpy as np
 import os
-import shutil
 import wfdb
+import csv
 
 recordList = wfdb.get_record_list('slpdb')
 apneaLabels = np.array(['H', 'HA', 'OA', 'CA', 'CAA', 'X'])
-allData = []
-index = 0
+#dataListOfLists = []
+inputArray = np.empty((0,1))
+outputArray = np.empty((0,1))
 
 for record in recordList:
-    print(record)
+
     #Read the annotations 
-    annsamp = wfdb.rdann(os.getcwd() + '\\data\\mit-bih-polysomnographic-database-1.0.0\\' + record, extension='st', summarize_labels=True)
+    annsamp = wfdb.rdann(os.getcwd() + '\\data\\mit-bih-polysomnographic-database-1.0.0\\' + record, extension='st', summarize_labels=True) #annsamp = wfdb.rdann(os.getcwd() + '/data/mit-bih-polysomnographic-database-1.0.0/' + record, extension='st', summarize_labels=True)
     
     #Read the data, create a Record
-    sig = wfdb.rdrecord(os.getcwd() + '\\data\\mit-bih-polysomnographic-database-1.0.0\\' + record, channels=[2])
-
-    startAnn = annsamp.sample[0]
-
+    sig = wfdb.rdrecord(os.getcwd() + '\\data\\mit-bih-polysomnographic-database-1.0.0\\' + record, channels=[2]) #sig = wfdb.rdrecord(os.getcwd() + '/data/mit-bih-polysomnographic-database-1.0.0/' + record, channels=[2])
+    
     actualPSignal = sig.p_signal
-    print(actualPSignal)
-    fs = sig.fs #sampling rate is 250 Hz
-
-    dataList = []
-
-
-    for idx, event in enumerate(annsamp.aux_note):
+    
+    #binarize data: 1 for a sleep apnea event in the epoch
+    for idx, event in enumerate(annsamp.aux_note): 
         for x in apneaLabels:
             if x in event:
                 annsamp.aux_note[idx] = 1
@@ -34,20 +33,29 @@ for record in recordList:
             else:
                 annsamp.aux_note[idx] = 0
 
+    #remove unannotated epochs from start of record
     numberEpochs = len(actualPSignal)/7500
-    signalEpochs = np.split(actualPSignal, numberEpochs)
     if numberEpochs != len(annsamp.aux_note):
-        print('Annotations:', len(annsamp.aux_note))
-        print('Epochs:', numberEpochs)
+        startingIndex = int(numberEpochs - len(annsamp.aux_note))
+        actualPSignal = actualPSignal[startingIndex*7500:]
 
-    #ERROR: annsamp.aux_note[i] is causing a 'list index out of range' error. It does not happen for every 
-    #record, it occurs first at record number 7 (slp14). CAUSE: This record has 720 epochs and only 714 annotations. 
-    #From ploting the data it seems that these missing annotations are at the start of the record. Also occurs for
-    #slp16, slp37, slp59, slp61 and slp66.
+    #add all epochs to an array, row by row
+    trimmedNumberEpochs = len(actualPSignal)/7500
+    epochs = np.split(actualPSignal, trimmedNumberEpochs)
+    index = 0
 
-    # i = 0
-    # for epoch in signalEpochs:
-    #     dataList.append([[epoch], [annsamp.aux_note[i]], [index]])
-    #     i+=1
-    #     index+=1
-    # print(i)
+    for epoch in epochs:
+        #create input/output arrays to CNN with shapes (numberEpochs, steps, channels) and (classification) respectively
+        #inputArray = np.vstack([inputArray, epoch])
+        #outputArray = np.vstack([outputArray, annsamp.aux_note[index]])
+        index = index + 1
+
+        #save input and output arrays as csv files
+        with open(os.getcwd() + '\\data\\inputs\\' + str(index) + '.csv','w') as filehandler: #/data/inputs/
+            print('writing file')
+            csvWriter = csv.writer(filehandler,delimiter=' ')
+            csvWriter.writerows(epoch)
+
+        #with open("/home/anaconda3/envs/sleep-apnea/data/targets/" + str(index) + ".csv","w") as filehandler:
+        #    csvWriter = csv.writer(filehandler,delimiter=' ')
+        #    csvWriter.writerows(outputArray)
