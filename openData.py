c5780304530348a7948e3b2bda9836cb8d0f939b
Lucia Quirke <lqui376@aucklanduni.ac.nz>
2019-08-15 12:02:10 +1200

    removed W from sleep stages# This is a combination of 2 commits.
    Removed W from sleep stages
    
    removed W from sleep stages

--- openData.py
+++ openData.py
@@ -15,27 +15,26 @@ operatingSystem = 'macOS'
 if operatingSystem is 'linux' or 'macOS':
     dataPath = '/data/mit-bih-polysomnographic-database-1.0.0/'
     inputsPath = '/data/inputs/'
-    targetsPath = '/data/6-shot-targets/'
+    targetsPath = '/data/5-shot-targets/'
 else:
     dataPath = '\\data\\mit-bih-polysomnographic-database-1.0.0\\'
     inputsPath = '\\data\\inputs\\'
-    targetsPath = "\\data\\6-shot-targets\\"
+    targetsPath = "\\data\\5-shot-targets\\"
 
 recordList = wfdb.get_record_list('slpdb')
 
-annotationDict = defaultdict(lambda: 6, {
+annotationDict = defaultdict(lambda: 5, {
     'H': 0,
     'HA': 0,
     'OA': 0,
     'CA': 0,
     'CAA': 0,
     'X': 0,
-    'W': 1,
-    '1': 2,
-    '2': 3,
-    '3': 4,
-    '4': 4,
-    'R': 5
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 3,
+    'R': 4
 })
 
 for recordNumber, record in enumerate(recordList):
@@ -58,13 +57,13 @@ for recordNumber, record in enumerate(recordList):
     # standardise signal
     actualPSignal = preprocessing.scale(actualPSignal)
 
-    # 6 shot classification target: first digit is apnea, next five are W, N1, N2, N3, and REM
-    target = np.zeros((numberAnnotations, 6)).astype(int)
+    # 5 shot classification target: first digit is apnea, next four are N1, N2, N3, and REM
+    target = np.zeros((numberAnnotations, 5)).astype(int)
 
     for i, note in enumerate(annsamp.aux_note):
         labels = note.split(' ')
         for label in labels:
-            if annotationDict[label] is not 6:
+            if annotationDict[label] is not 5:
                 target[i, annotationDict[label]] = 1
 
     # write each epoch to a csv file, named by record number and epoch number
