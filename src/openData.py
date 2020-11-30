4f294213098b68b002b53b2f3512e58225dd0c54
Lucia Quirke <luciarosequirke@gmail.com>
2020-11-30 18:24:00 +1300

    Replace os library with pathlib
    - remove references to operating system (handled natively by pathlib)
    - refactor folder setup function
    - rename actualPSignal -> physical_signal and sig -> signal for consistency
    - update use of 'is not' on literals to '!='

--- src/openData.py
+++ src/openData.py
@@ -1,13 +1,12 @@
 import numpy as np
 import os
+from pathlib import Path
 import wfdb
 from collections import defaultdict
 from sklearn import preprocessing
 
 def main() -> None:
     """Loads relevant data from PhysioBank using wfdb package specified in documentation and saves it to folders"""
-    # change this variable to 'macOS', 'linux', or 'windows'
-    operating_system = 'macOS'
 
     annotation_dict = defaultdict(lambda: 5, {
         '1': 0,
@@ -37,18 +36,17 @@ def main() -> None:
     record_list = wfdb.get_record_list('slpdb')
 
     for record_index, record in enumerate(record_list):
-
         # read the annotations and data then create a record
-        annsamp = wfdb.rdann(os.getcwd() + data_path + record, extension='st', summarize_labels=True)
-        sig = wfdb.rdrecord(os.getcwd() + data_path + record, channels=[2])
-        actualPSignal = sig.p_signal
-        actualPSignal = preprocessing.scale(actualPSignal)
+        annsamp = wfdb.rdann(str(data_path / record), extension='st', summarize_labels=True)
+        signal = wfdb.rdrecord(str(data_path / record), channels=[2])
+        physical_signal = signal.p_signal
+        physical_signal = preprocessing.scale(physical_signal)
 
         # remove unannotated epochs (30 second input segments) from the start of the record and split into inputs
         number_annotations = len(annsamp.aux_note)
-        starting_index = int((len(actualPSignal) / 7500) - number_annotations)*7500
-        actualPSignal = actualPSignal[starting_index:]
-        inputs = np.split(actualPSignal, number_annotations)
+        starting_index = int((len(physical_signal) / 7500) - number_annotations)*7500
+        physical_signal = physical_signal[starting_index:]
+        inputs = np.split(physical_signal, number_annotations)
 
         # generate each 5 shot classification target as 0000: N1, N2, N3, REM, and wake
         target = [[0]*4 for _ in range(number_annotations)]
@@ -57,15 +55,15 @@ def main() -> None:
         for annotation_index in range(number_annotations):
             labels = annsamp.aux_note[annotation_index].split(' ')
             for label in labels:
-                if annotation_dict[label] is not 5:
+                if annotation_dict[label] != 5:
                     target[annotation_index][annotation_dict[label]] = 1
 
             # write each input to a csv file, named by record number and input number
-            with open(os.getcwd() + inputs_path + str(record_index) + '_' + str(annotation_index) + '.csv',
+            with open(str(inputs_path / (str(record_index) + '_' + str(annotation_index) + '.csv')),
                       'w') as filehandler:
                 filehandler.write("\n".join(str(num) for num in inputs[annotation_index]))
 
-            with open(os.getcwd() + targets_path + str(record_index) + '_' + str(annotation_index) + ".csv", "w") as \
+            with open(str(targets_path / (str(record_index) + '_' + str(annotation_index) + ".csv")), "w") as \
                     fileHandler:
                 event_class = ''.join([str(v) for v in target[annotation_index]])
                 class_count[event_class] += 1
@@ -83,27 +81,16 @@ def main() -> None:
 
 def folder_setup() -> Tuple[str, str, str]:
     """Specifies the folder structures and pathways, creating any missing folders"""
-    if OS is 'linux' or OS is 'macOS':
-        data_path = '/data/mit-bih-polysomnographic-database-1.0.0/'
-        inputs_path = '/data/inputs/'
-        targets_path = '/data/5-shot-targets/'
-        top_folder = '/data'
-    else:
-        data_path = '\\data\\mit-bih-polysomnographic-database-1.0.0\\'
-        inputs_path = '\\data\\inputs\\'
-        targets_path = "\\data\\5-shot-targets\\"
-        top_folder = '\\data'
+    data_path = Path('data/mit-bih-polysomnographic-database-1.0.0/')
+    inputs_path = Path('data/inputs/')
+    targets_path = Path('data/5-shot-targets/')
 
-    if not os.path.isdir('.' + top_folder):
-        os.mkdir('.' + top_folder)
-    if not os.path.isdir('.' + data_path):
-        os.mkdir('.' + data_path)
-        print("ensure database downloaded and in working directory in data_path folder")
-    if not os.path.isdir('.' + inputs_path):
-        os.mkdir('.' + inputs_path)
-        os.mkdir('.' + targets_path)
+    data_path.mkdir(parents=True, exist_ok=True)
+    inputs_path.mkdir(parents=True, exist_ok=True)
+    targets_path.mkdir(parents=True, exist_ok=True)
+    print("ensure database downloaded from https://physionet.org/content/slpdb/1.0.0/ and in working directory in data_path folder")
 
-    return inputs_path, targets_path, data_path
+    return inputs_path.absolute(), targets_path.absolute(), data_path.absolute()
 
 
 # starts main if file called as script (rather than imported)
